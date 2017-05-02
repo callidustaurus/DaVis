c691a3e43bb031b413c96496bf6553c227c45d71
Stefan Matzdorf <stefan.matzdorf@springer.com>
2017-05-02 18:33:12 +0200

    fill SpkData with proper annotations to map the spk-csv format

--- app/de/stema/finance/SpkData.scala
+++ app/de/stema/finance/SpkData.scala
@@ -1,10 +1,58 @@
 package de.stema.finance
 
+import java.time.LocalDate
+
 import de.stema.io.CSVData
 
 class SpkData {
-  @CSVData("Auftragskonto") val konto: String = null
-  @CSVData("Buchungstag") val buchungstag: String = null
-  @CSVData("Verwendungszweck") val verwendungszweck: String = null
-  @CSVData("Betrag") val betrag: String = null
+  @CSVData("Auftragskonto")
+  val aufragsKonto: BigInt = null
+
+  @CSVData("Buchungstag")
+  val buchungsTag: LocalDate = null
+
+  @CSVData("Valutadatum")
+  val valutaDatum: LocalDate = null
+
+  @CSVData("Buchungstext")
+  val buchungsText: String = null
+
+  @CSVData("Verwendungszweck")
+  val verwendungszweck: String = null
+
+  @CSVData("Glaeubiger ID")
+  val glaeubigerID: String = null
+
+  @CSVData("Mandatsreferenz")
+  val mandatsReferenz: String = null
+
+  @CSVData("Kundenreferenz (End-to-End)")
+  val kundenReferenz: String = null
+
+  @CSVData("Sammlerreferenz")
+  val sammlerReferenz: String = null
+
+  @CSVData("Lastschrift Ursprungsbetrag")
+  val ursprungsBetrag: String = null
+
+  @CSVData("Auslagenersatz Ruecklastschrift")
+  val auslagenErsatzRuecklastschrift: String = null
+
+  @CSVData("Beguenstigter/Zahlungspflichtiger")
+  val beguenstigter: String = null
+
+  @CSVData("Kontonummer/IBAN")
+  val iban: String = null
+
+  @CSVData("BIC (SWIFT-Code)")
+  val bic: String = null
+
+  @CSVData("Betrag")
+  val betrag: BigDecimal = null
+
+  @CSVData("Waehrung")
+  val waehrung: String = null
+
+  @CSVData("Info")
+  val info: String = null
 }
